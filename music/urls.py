3aec05accfb89345c3c1c398060d205e08891b05
unyil <1318360+unyil@users.noreply.github.com>
2018-12-13 11:45:14 +0700

    Upgrade code to Django 2.1.4

--- music/urls.py
+++ music/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^home$', views.home, name="home"), 
     url(r'^edit/(?P<pk>\d+)$', views.edit, name='edit'),
     url(r'^delete/(?P<pk>\d+)$', views.delete, name='delete'), 
-    url(r'^$', auth_views.login, {'template_name': 'music/login.html'}, name='login'),
-    url(r'^logout', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^login/$',auth_views.LoginView.as_view(template_name="music/login.html"), name="login"),
+    url(r'^logout', auth_views.LogoutView, {'next_page': 'login'}, name='logout'),
     url(r'^signup$', views.signup, name='signup'),
 ]
